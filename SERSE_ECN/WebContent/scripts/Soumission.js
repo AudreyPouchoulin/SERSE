894b7c2d1aab1ed7f6c7c01b4bd289ed80920534
AudreyPouchoulin <audrey.pouchoulin@gmail.com>
2014-03-18 16:38:41 +0100

    Vérification des champs avant soumission ok

--- SERSE_ECN/WebContent/scripts/Soumission.js
+++ SERSE_ECN/WebContent/scripts/Soumission.js
@@ -7,9 +7,9 @@ function resetFormulaire() {
 	$('#continent').val("defaut");
 	$('#pays').val("defaut");
 	$('#ville').val("defaut");
-	$('#sejour').val("defaut");
-	$('#mobilite').val("defaut");
-	$('#experience').val("defaut");
+	$('#typeSejour').val("defaut");
+	$('#typeMobilite').val("defaut");
+	$('#typeExperience').val("defaut");
 	$('#universite_entreprise').val("defaut");
 	$('#langue').val("defaut");
 	$('#domaine').val("defaut");
@@ -32,14 +32,14 @@ function verifierEntrees() {
 	var $messageListeErreurs = '';
 
 	// erreur sur les champs dates
-	if ($('#dateDebut').text() == '') {
+	if ($('#dateDebut').val() == '') {
 		$messageListeErreurs = $messageListeErreurs
 				+ "Merci d'entrer une date de début.\n";
 	} else if (!(isFormatDate($('#dateDebut')))) {
 		$messageListeErreurs = $messageListeErreurs
 				+ "Merci d'entrer une date de début de séjour au format jj/mm/aaaa.\n";
 	}
-	if ($('#dateFin').text() == '') {
+	if ($('#dateFin').val() == '') {
 		$messageListeErreurs = $messageListeErreurs
 				+ "Merci d'entrer une date de fin.\n";
 	} else if (!(isFormatDate($('#dateFin')))) {
@@ -50,10 +50,10 @@ function verifierEntrees() {
 	// erreur sur le champs continent, pays, ville
 	$messageListeErreurs = testChampsDefaut($('#continent'),
 			"Merci d'entrer un continent.", $messageListeErreurs);
-	$messageListeErreurs = testChampsDefautComplex($('#pays'), $('#autrePays'),
+	$messageListeErreurs = testChampsDefautComplex($('#pays'), $('#valueAutrePays'),
 			"Merci d'entrer un pays.", $messageListeErreurs);
 	$messageListeErreurs = testChampsDefautComplex($('#ville'),
-			$('#autreVille'), "Merci d'entrer une ville.", $messageListeErreurs);
+			$('#valueAutreVille'), "Merci d'entrer une ville.", $messageListeErreurs);
 
 	// erreur sur les champs types de séjour, type de mobilité, type
 	// d'expérience
@@ -70,12 +70,12 @@ function verifierEntrees() {
 				+ "Merci d'entrer le nom d'une université ou entreprise.\n";
 	} else if ($('#universite_entreprise #universiteGroup option:selected')
 			.text() == 'Autre'
-			&& $('#autreUniversite').text() == '') {
+			&& $('#valueAutreUniversite').val() == '') {
 		$messageListeErreurs = $messageListeErreurs
 				+ "Merci d'entrer le nom d'une université.\n";
 	} else if ($('#universite_entreprise #entrepriseGroup option:selected')
 			.text() == 'Autre'
-			&& $('#autreEntreprise').text() == '') {
+			&& $('#valueAutreEntreprise').val() == '') {
 		$messageListeErreurs = $messageListeErreurs
 				+ "Merci d'entrer le nom d'une entreprise. \n";
 	}
@@ -89,12 +89,16 @@ function verifierEntrees() {
 			$messageListeErreurs);
 
 	// erreur sur le champs adresse, coordonnées GPS, chemin du fichier
-	if ($('#adresse').text() == '') {
+	if ($('#adresse').val() == '') {
 		$messageListeErreurs = $messageListeErreurs
 				+ "Merci d'entrer le nom d'une adresse, et les coordonnées GPS (en cliquant sur le bouton correspondant).\n";
 	}
 	// TODO : GPS vérification
-	if ($('#cheminFichier').text() == '') {
+	/*if ($('#coordonnees').val() == '') {
+		$messageListeErreurs = $messageListeErreurs
+				+ "Merci d'entrer les coordonnées GPS (en cliquant sur le bouton correspondant).\n";
+	}*/
+	if ($('#cheminFichier').val() == '') {
 		$messageListeErreurs = $messageListeErreurs
 				+ "Merci d'entrer le chemin du fichier à transmettre.\n";
 	}
@@ -115,25 +119,26 @@ function recupererArguments() {
 			$('#autreLangue'));
 	var $domaine = recupererValeurAvecPossibiliteAutre($('#domaine'),
 			$('#autreDomaine'));
+	//TODO : ajouter la récupération du GPS
 
 	var arguments = {
 		Nom : $('#nom').text(),
 		Prenom : $('#prenom').text(),
 		Sexe : $('#sexe').text(),
-		DateDebut : $('#dateDebut').text(),
-		DateFin : $('#dateFin').text(),
-		Continent : $('#continent').text(),
+		DateDebut : $('#dateDebut').val(),
+		DateFin : $('#dateFin').val(),
+		Continent : $('#continent').val(),
 		Pays : $pays,
 		Ville : $ville,
-		Sejour : $('#typeSejour').text(),
-		Mobilite : $('#typeMobilite').text(),
-		Experience : $('#typeExperience').text(),
+		Sejour : $('#typeSejour').val(),
+		Mobilite : $('#typeMobilite').val(),
+		Experience : $('#typeExperience').val(),
 		Universite : $universite,
 		Entreprise : $entreprise,
 		Langue : $langue,
 		Domaine : $domaine,
-		Adresse : $('#adresse').text(),
-		CheminFichier : $('#cheminFichier').text()
+		Adresse : $('#adresse').val(),
+		CheminFichier : $('#cheminFichier').val()
 	};
 	return arguments;
 }
@@ -147,18 +152,17 @@ function testChampsDefaut(champs, messageErreur, messageListeErreurs) {
 
 function testChampsDefautComplex(champs, autreChamps, messageErreur,
 		messageListeErreurs) {
-	alert(autreChamps.val() == '');
-	if (champs.val() == 'defaut' || (champs.val() == 'Autre' && autreChamps.text() == '')) {
+	if (champs.val() == 'defaut' || (champs.val() == 'Autre' && autreChamps.val() == '')) {
 		messageListeErreurs = messageListeErreurs + messageErreur + "\n";
 	}
 	return messageListeErreurs;
 }
 
 function recupererValeurAvecPossibiliteAutre(champs, champsAutre) {
-	if (champs.text() == 'Autre') {
-		$pays = champsAutre.text();
+	if (champs.val() == 'Autre') {
+		$pays = champsAutre.val();
 	} else {
-		$pays = champs.text();
+		$pays = champs.val();
 	}
 }
 /**
@@ -171,9 +175,9 @@ function isFormatDate(entreeUtilisateur) {
 	var amin = 2010; // année mini
 	var amax = 2100; // année maxi
 	var separateur = "/"; // separateur entre jour/mois/annee
-	var j = (d.substring(0, 2));
-	var m = (d.substring(3, 5));
-	var a = (d.substring(6));
+	var j = (entreeUtilisateur.val().substring(0, 2));
+	var m = (entreeUtilisateur.val().substring(3, 5));
+	var a = (entreeUtilisateur.val().substring(6));
 	if ((isNaN(j)) || (j < 1) || (j > 31)) {
 		isGoodFormat = false;
 	}
@@ -183,7 +187,7 @@ function isFormatDate(entreeUtilisateur) {
 	if ((isNaN(a)) || (a < amin) || (a > amax)) {
 		isGoodFormat = false;
 	}
-	if ((d.substring(2, 3) != separateur) || (d.substring(5, 6) != separateur)) {
+	if ((entreeUtilisateur.val().substring(2, 3) != separateur) || (entreeUtilisateur.val().substring(5, 6) != separateur)) {
 		isGoodFormat = false;
 	}
 	return isGoodFormat;
